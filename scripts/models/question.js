35ad936d2fb4f646d16cf0d09c765a1d729f5a44
Brian Genisio <BrianGenisio@Gmail.com>
2012-12-28 21:45:13 -0500

    Constrain the vote

--- scripts/models/question.js
+++ scripts/models/question.js
@@ -3,9 +3,28 @@ App.Models.Question = Backbone.Model.extend({
 		votes: []
 	},
 
+	constrain: function(value, min, max) {
+		if(value < min) return min;
+		if(value > max) return max;
+		return value;
+	},
+
+	getExistingVote: function(userName) {
+		return _(this.get('votes')).find(function(vote) {
+			return vote.voter == userName;
+		});
+	},
+
 	vote: function(userName, voteType) {
 		var voteValue = voteType === 'up' ? 1 : -1;
-		this.get('votes').push({ value: voteValue, voter: userName });
+		var existingVote = this.getExistingVote(userName);
+
+		if(existingVote) {
+			existingVote.value = this.constrain(existingVote.value + voteValue, -1, 1);
+		} else {
+			this.get('votes').push({ value: voteValue, voter: userName });
+		}
+		
 		this.save();
 	},
 
