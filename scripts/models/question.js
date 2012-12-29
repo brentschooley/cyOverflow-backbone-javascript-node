f16d861a7196b1945b7e2b27d57e5a059c0745ad
Brian Genisio <BrianGenisio@Gmail.com>
2012-12-28 21:47:59 -0500

    Indicate the vote

--- scripts/models/question.js
+++ scripts/models/question.js
@@ -24,7 +24,7 @@ App.Models.Question = Backbone.Model.extend({
 		} else {
 			this.get('votes').push({ value: voteValue, voter: userName });
 		}
-		
+
 		this.save();
 	},
 
@@ -32,6 +32,16 @@ App.Models.Question = Backbone.Model.extend({
 		return this.get('votes').reduce( function(tally, vote) {
 			return tally + vote.value;
 		}, 0 );
-	}
+	},
+
+	didUserVote: function(userName, voteValue) {
+		var vote = this.getExistingVote(userName);
+		return !!(vote && vote.value == voteValue);
+	},
+
+	didUserVoteUp:   function(userName) { return this.didUserVote(userName, 1);  },
+	didUserVoteDown: function(userName) { return this.didUserVote(userName, -1); }
+ 
+
         
 });
